817029c1bd5691b7de81ca75d91c7f0c89761a15
moinocencio <moinocencio@gmail.com>
2019-09-07 03:44:13 +0100

    Basic Waterflow sensor Demo

--- node-mcu-sh19/src/waterflow_demo.cpp
+++ node-mcu-sh19/src/waterflow_demo.cpp
@@ -3,19 +3,20 @@
 #include "waterflow.h"
 #include "wifiSH.h"
 #include "publisherSH.h"
+
 #include <ESP8266WiFi.h>
 
 /** Water flow demo:
  * Basic demo for interaction with NODE <-> DB
 **/
 
-#define WATERFLOWADC_PIN A0
+#define WATERFLOWADC_PIN A0                         // ADC read pin
 
-Waterflow sensorUnit = Waterflow();
-uint8 sensID = 42;
+Waterflow sensorUnit = Waterflow();                 
+uint8 sensID = 42;                                  // Unit ID
 
-uint32_t total = 0;
-uint32_t measure;
+uint32_t total = 0;                                 // Total water spent in utilization
+uint32_t measure;                                   // Instantaneous measurement
 
 #ifndef PLATFORM_HOST
     #define PLATFORM_HOST "192.168.63.237"
@@ -26,6 +27,9 @@ char *uuid = "heck";
 char endpoint[80];
 
 PublisherSH waterFlowPublisher = PublisherSH();
+
+uint32_t timePerUtilization = 60000;
+unsigned long previousT, currentT;
 
 void setup() {
     Serial.begin(115200);
@@ -43,10 +47,20 @@ void setup() {
 void loop() {
 
     delay(500);
-    measure = sensorUnit.return_data();        
+    measure = sensorUnit.return_data();
+    Serial.println(measure);
 
     if(measure > 50) {
-        total += measure;
+        total = 0;
+        previousT = millis();
+
+        while (millis() < (previousT + timePerUtilization) && measure > 50)
+        {
+            total += measure;
+            measure = sensorUnit.return_data();
+            Serial.println(measure);
+            delay(200);
+        }                
 
         Serial.println(total);
 
